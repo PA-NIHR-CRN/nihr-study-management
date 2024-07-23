7881dc46ebbaa12519beb2e42042a369702de3f6
P Gadzios <pgadzios@leftturnsolutions.com>
2024-07-23 10:36:38 +0100

    Initial WIP MVC site

---
+++ NIHR.StudyManagement/NIHR.StudyManagement.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NIHR.StudyManagement.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
