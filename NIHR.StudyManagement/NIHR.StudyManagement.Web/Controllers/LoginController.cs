c51fc3dd35b29b7a0eaea97e375084137f410096
eliatanasova <elitsaargirova@hotmail.com>
2024-07-24 14:19:13 +0100

    Log in page frontend completed.

---
+++ NIHR.StudyManagement/NIHR.StudyManagement.Web/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using NIHR.StudyManagement.Web.Models;
+using System.Diagnostics;
+
+namespace NIHR.StudyManagement.Web.Controllers;
+
+public class LoginController : Controller
+{
+    private readonly ILogger<LoginController> _logger;
+
+    public LoginController(ILogger<LoginController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    [HttpGet]
+    public IActionResult Privacy2()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
